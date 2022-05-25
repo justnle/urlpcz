d7db4017e6d15e0d26766e9c56360d154c69146c
Justin <info@justinle.me>
2022-05-24 18:55:43 -0700

    controller init

---
+++ server/src/controller/shortUrl.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+import shortUrl from '../models/shortUrl.model';
+
+export async function createShortUrl(req: Request, res: Response) {
+    const { destination } = req.body;
+
+    const newUrl = await shortUrl.create({ destination });
+
+    return res.send(newUrl);
+}
