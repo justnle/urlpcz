e8d9f10af95ad1b92d1fb40f43536ebc85471f02
Justin <info@justinle.me>
2022-06-14 17:35:42 -0700

    added validator and analytics

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,17 +1,22 @@
 import { Express, Request, Response } from 'express';
 import {
     createShortUrl,
-    handleRedirect
+    handleRedirect,
+    getAnalytics
 } from '../controller/shortUrl.controller';
+import validateResource from '../middleware/validateResource';
+import shortUrlSchema from '../schemas/createShortUrl.schema';
 
 function routes(app: Express) {
     app.get(`/healthcheck`, (req: Request, res: Response) => {
         return res.send(`App is healthy`);
     });
 
-    app.post(`/api/url`, createShortUrl);
+    app.post(`/api/url`, validateResource(shortUrlSchema), createShortUrl);
 
     app.get(`/:shortId`, handleRedirect);
+
+    app.get(`/api/analytics`, getAnalytics);
 }
 
 export default routes;
